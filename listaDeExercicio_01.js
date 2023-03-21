f2e984b5d95e808ee3167da16593f1f6c37b199d
pedro <pedro0123passos@gmail.com>
2023-03-21 16:29:20 -0300

    separando o codigo em função

--- listaDeExercicio_01.js
+++ listaDeExercicio_01.js
@@ -10,22 +10,14 @@
     -Média acima de 7, passou de semestre
 */
 
-
-let notas = [7, 7, 8]; // usei uma array para colocar as 3 notas, mas eu tbm poderia declara 3 variaveis para armazenar as notas;
-let media = ((notas[0] + notas[1] + notas[2]) / notas.length).toFixed(2)
-if (media > 7) {
-    console.log(`Passou de Semestre a media do aluno foi ${media}`);
-} else if (media <= 7 && media >= 5) {
-    console.log(`Recuperação a media do aluno foi ${media}`);
-} else {
-    console.log(`Reprovado a media do aluno foi ${media}`);
-}
-
-
-
-
-
-
-
-//console.log(media.toFixed(2))
-
+(function main() {
+    let notas = [7, 7, 8]; // usei uma array para colocar as 3 notas, mas eu tbm poderia declara 3 variaveis para armazenar as notas;
+    let media = ((notas[0] + notas[1] + notas[2]) / notas.length).toFixed(2)
+    if (media > 7) {
+        console.log(`Passou de Semestre a media do aluno foi ${media}`);
+    } else if (media <= 7 && media >= 5) {
+        console.log(`Recuperação a media do aluno foi ${media}`);
+    } else {
+        console.log(`Reprovado a media do aluno foi ${media}`);
+    }
+})();
